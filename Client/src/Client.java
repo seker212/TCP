c75a72acb1d371de3e93ec9d56959b17e725d6d3
seker212 <seker212@gmail.com>
2019-10-31 21:45:05 +0100

    New core verion form unknown link xD

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,58 +1,103 @@
-// Java implementation for a client 
-// Save file as Client.java 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
-import java.io.*; 
-import java.net.*; 
-import java.util.Scanner; 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-// Client class 
-public class Client 
-{ 
-	public static void main(String[] args) throws IOException 
-	{ 
-		try
-		{ 
-			Scanner scn = new Scanner(System.in); 
-			
-			// getting localhost ip 
-			InetAddress ip = InetAddress.getByName("localhost"); 
-	
-			// establish the connection with server port 5056 
-			Socket s = new Socket(ip, 5056); 
-	
-			// obtaining input and out streams 
-			DataInputStream dis = new DataInputStream(s.getInputStream()); 
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-	
-			// the following loop performs the exchange of 
-			// information between client and client handler 
-			while (true) 
-			{ 
-				System.out.println(dis.readUTF()); 
-				String tosend = scn.nextLine(); 
-				dos.writeUTF(tosend); 
-				
-				// If client sends exit,close this connection 
-				// and then break from the while loop 
-				if(tosend.equals("Exit")) 
-				{ 
-					System.out.println("Closing this connection : " + s); 
-					s.close(); 
-					System.out.println("Connection closed"); 
-					break; 
-				} 
-				
-				// printing date or time as requested by client 
-				String received = dis.readUTF(); 
-				System.out.println(received); 
-			} 
-			
-			// closing resources 
-			scn.close(); 
-			dis.close(); 
-			dos.close(); 
-		}catch(Exception e){ 
-			e.printStackTrace(); 
-		} 
-	} 
-} 
+/**
+ * A simple Swing-based client for the chat server. Graphically it is a frame with a text
+ * field for entering messages and a textarea to see the whole dialog.
+ *
+ * The client follows the following Chat Protocol. When the server sends "SUBMITNAME" the
+ * client replies with the desired screen name. The server will keep sending "SUBMITNAME"
+ * requests as long as the client submits screen names that are already in use. When the
+ * server sends a line beginning with "NAMEACCEPTED" the client is now allowed to start
+ * sending the server arbitrary strings to be broadcast to all chatters connected to the
+ * server. When the server sends a line beginning with "MESSAGE" then all characters
+ * following this string should be displayed in its message area.
+ */
+public class Client {
+
+    String serverAddress;
+    Scanner in;
+    PrintWriter out;
+    JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(50);
+    JTextArea messageArea = new JTextArea(16, 50);
+
+    /**
+     * Constructs the client by laying out the GUI and registering a listener with the
+     * textfield so that pressing Return in the listener sends the textfield contents
+     * to the server. Note however that the textfield is initially NOT editable, and
+     * only becomes editable AFTER the client receives the NAMEACCEPTED message from
+     * the server.
+     */
+    public Client(String serverAddress) {
+        this.serverAddress = serverAddress;
+
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.SOUTH);
+        frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
+        frame.pack();
+
+        // Send on enter then clear to prepare for next message
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
+            }
+        });
+    }
+
+    private String getName() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Choose a screen name:",
+            "Screen name selection",
+            JOptionPane.PLAIN_MESSAGE
+        );
+    }
+
+    private void run() throws IOException {
+        try {
+            Socket socket = new Socket(serverAddress, 59001);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (line.startsWith("SUBMITNAME")) {
+                    out.println(getName());
+                } else if (line.startsWith("NAMEACCEPTED")) {
+                    this.frame.setTitle("Chatter - " + line.substring(13));
+                    textField.setEditable(true);
+                } else if (line.startsWith("MESSAGE")) {
+                    messageArea.append(line.substring(8) + "\n");
+                }
+            }
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
+        }
+        Client client = new Client(args[0]);
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
+}
